1f321fd972294338226467158585ec27a8334ce7
tanish-sd26 <mauryatanisha824@gmail.com>
2026-02-05 17:13:56 +0530

    Create sentence maker.js

---
+++ sentence maker.js
@@ -0,0 +1,19 @@
+let adjective = "beautiful" ;
+let noun = "girl" ;
+let verb = "dance" ;
+let place = "paris" ;
+let adjective2 = "big" ;
+let noun2 = "cake" ; 
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun +" lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " +firstStory);
+
+adjective = "dangerous" ;
+noun = "dragon" ;
+verb = "fly" ;
+place = "china" ;
+adjective2 = "biggest" ;
+noun2 = "meat" ;
+
+const secondStory = "Once upon a time, there was a " + adjective + " " +noun + " who loved to eat " +noun2 + "." + " The " + noun +" lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb+ ".";
+console.log("Second Story:" +secondStory);
