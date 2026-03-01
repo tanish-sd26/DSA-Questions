6fae2f375cac3a8dc4a7f1a9bbf3ffff54edc878
tanish-sd26 <mauryatanisha824@gmail.com>
2026-03-02 00:50:56 +0530

    Update teacherChatbot.js

--- teacherChatbot.js
+++ teacherChatbot.js
@@ -9,3 +9,6 @@ const subject = "JavaScript";
 const topic = "strings";
 const sentence = `Today, you will learn about ${topic} in ${subject}.`;
 console.log(sentence);
+
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
