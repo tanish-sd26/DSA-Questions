32685239b4b99867815d58fc8d673a8b3d1188e2
tanish-sd26 <mauryatanisha824@gmail.com>
2026-03-02 16:55:13 +0530

    Add function within function

--- try.js
+++ try.js
@@ -9,7 +9,17 @@ let str7 = "A";
 let result = str1 + str2 + str3 + str4 + str5 + str6 + str7;
 console.log (result);
 
-let f = 'x';
+let f0 = 'x';
 let f1 = 'y';
-let f2 = f1+ " " + f;
+let f2 = f1+ " " + f0;
 console.log(f2);
+//fuctions with in functions
+function createFunction() {
+    function f(a, b) {
+        const sum = a + b;
+        return sum;
+    }
+    return f;
+}
+const f = createFunction();
+console.log(f(3, 4)); // 7
