40b9f7e6c224989454cf247373bc52703c562884
tanish-sd26 <mauryatanisha824@gmail.com>
2026-03-04 23:53:14 +0530

    Create leetcode.js start with function composition

---
+++ Leetcode.js
@@ -0,0 +1,12 @@
+// 2679. Function Composition
+function compose(functions) {
+    return function(x) {
+        let result = x;
+
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+
+        return result;
+    };
+}
