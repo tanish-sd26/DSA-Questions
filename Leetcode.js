6995eb04ccb936b154c0ffc8c56d8bac510bbbad
tanish-sd26 <mauryatanisha824@gmail.com>
2026-03-05 22:23:52 +0530

    memoize function

--- Leetcode.js
+++ Leetcode.js
@@ -29,3 +29,21 @@ function once(fn) {
         return fn(...args);
     };
 }
+
+//function fn, return a memoized version of that function
+function memoize(fn) {
+    const cache = {};
+
+    return function(...args) {
+        const key = args.join(",");
+
+        if (key in cache) {
+            return cache[key];
+        }
+
+        const result = fn(...args);
+        cache[key] = result;
+
+        return result;
+    };
+}
