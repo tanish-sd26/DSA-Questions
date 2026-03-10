028a385f377b51e13efb2b1e296c5ee462cc0a9a
tanish-sd26 <mauryatanisha824@gmail.com>
2026-03-10 22:27:46 +0530

    Calculator class supporting chained operations

--- Leetcode.js
+++ Leetcode.js
@@ -417,3 +417,46 @@ class ArrayWrapper {
         return `[${this.nums.join(',')}]`;
     }
 }
+
+//Calculator with method chaining
+
+class Calculator {
+
+    constructor(value) {
+        this.result = value;
+    }
+
+    add(value) {
+        this.result += value;
+        return this;
+    }
+
+    subtract(value) {
+        this.result -= value;
+        return this;
+    }
+
+    multiply(value) {
+        this.result *= value;
+        return this;
+    }
+
+    divide(value) {
+
+        if (value === 0) {
+            throw new Error("Division by zero is not allowed");
+        }
+
+        this.result /= value;
+        return this;
+    }
+
+    power(value) {
+        this.result = this.result ** value;
+        return this;
+    }
+
+    getResult() {
+        return this.result;
+    }
+}
