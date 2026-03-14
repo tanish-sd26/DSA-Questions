55f821af4b75988c9726f25c4844a49c5e3c0890
tanish-sd26 <mauryatanisha824@gmail.com>
2026-03-14 18:16:24 +0530

    Solve Two sum problem

--- Leetcode.js
+++ Leetcode.js
@@ -634,3 +634,20 @@ Function.prototype.callPolyfill = function(obj, ...args) {
 
     return result;
 };
+
+// Find two indices whose values sum to target
+var twoSum = function(nums, target) {
+
+    const map = new Map(); // value -> index
+
+    for (let i = 0; i < nums.length; i++) {
+
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+};
