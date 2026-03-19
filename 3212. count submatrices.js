4e87b26374d6c7b218e219797d1f29b6b1d3e174
tanish-sd26 <mauryatanisha824@gmail.com>
2026-03-19 20:56:48 +0530

    Resolve multiple valid submatrices ending at (i,j)

--- 3212. count submatrices.js
+++ 3212. count submatrices.js
@@ -1,26 +1,39 @@
-// Count valid submatrices starting from (0,0)
+// Final correct solution
 var numberOfSubmatrices = function(grid) {
     
     let m = grid.length;
     let n = grid[0].length;
     
-    let countX = 0; // total X till current cell
-    let countY = 0; // total Y till current cell
-    
     let result = 0;
     
-    // Traverse entire grid
+    // Prefix for X and value
+    let colSum = new Array(n).fill(0);
+    let colX = new Array(n).fill(0);
+    
     for (let i = 0; i < m; i++) {
+        
         for (let j = 0; j < n; j++) {
             
-            // Update counts based on current cell
-            if (grid[i][j] === 'X') countX++;
-            else if (grid[i][j] === 'Y') countY++;
+            // Convert values
+            let val = grid[i][j] === 'X' ? 1 : (grid[i][j] === 'Y' ? -1 : 0);
             
-            // Check conditions:
-            // 1. equal X and Y
-            // 2. at least one X
-            if (countX === countY && countX > 0) {
+            colSum[j] += val;
+            
+            if (grid[i][j] === 'X') {
+                colX[j] += 1;
+            }
+        }
+        
+        let sum = 0;
+        let xCount = 0;
+        
+        for (let j = 0; j < n; j++) {
+            
+            sum += colSum[j];
+            xCount += colX[j];
+            
+            // valid submatrix from (0,0) to (i,j)
+            if (sum === 0 && xCount > 0) {
                 result++;
             }
         }
