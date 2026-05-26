5e9914b7e55ef6ff663236fcf78a1991be7b1855
tanish-sd26 <mauryatanisha824@gmail.com>
2026-05-26 13:59:44 +0530

    Create 14 .LongestCommonPrefix.js

---
+++ 14 .LongestCommonPrefix.js
@@ -0,0 +1,15 @@
+// Compare strings character by character and shrink prefix when mismatch found
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (!prefix) return "";
+        }
+    }
+
+    return prefix;
+};
