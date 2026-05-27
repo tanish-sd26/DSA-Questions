a0aa1b99cbff27d04165438845461750feac1ce8
tanish-sd26 <mauryatanisha824@gmail.com>
2026-05-27 14:09:43 +0530

    Use two pointers with a dummy node to remove nth node in one pass

---
+++ 19. removeNth Node.js
@@ -0,0 +1,20 @@
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+
+    let fast = dummy;
+    let slow = dummy;
+
+    for (let i = 0; i <= n; i++) {
+        fast = fast.next;
+    }
+
+    while (fast !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return dummy.next;
+};
