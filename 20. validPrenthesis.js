b8a32d1917084e3eaf84fed1437759dd708eacfd
tanish-sd26 <mauryatanisha824@gmail.com>
2026-05-27 14:13:14 +0530

    // Use stack to match opening and closing brackets

---
+++ 20. validPrenthesis.js
@@ -0,0 +1,20 @@
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else {
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
