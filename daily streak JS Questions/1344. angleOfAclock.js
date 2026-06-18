b3c196de302a349d0107946ce7cc763e2e44fe88
tanish-sd26 <mauryatanisha824@gmail.com>
2026-06-18 19:56:45 +0530

    feat: angle between hands of a clock

---
+++ daily streak JS Questions/1344. angleOfAclock.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} hour
+ * @param {number} minutes
+ * @return {number}
+ */
+var angleClock = function(hour, minutes) {
+
+    const hourAngle =
+        (hour % 12) * 30 +
+        minutes * 0.5;
+
+    const minuteAngle =
+        minutes * 6;
+
+    const diff =
+        Math.abs(hourAngle - minuteAngle);
+
+    return Math.min(
+        diff,
+        360 - diff
+    );
+};
