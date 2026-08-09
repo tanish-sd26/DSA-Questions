4fdfca9e9ab63dc837d78be67dafb514b5828bf0
tanish-sd26 <mauryatanisha824@gmail.com>
2026-08-09 11:19:22 +0530

    Day 23 of HackerRank : BST Level-Order Traversal

---
+++ HackerRank/30 Days challenge/day 23.js
@@ -0,0 +1,82 @@
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}; // End of function Node
+
+// Start of function BinarySearchTree
+function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);
+            
+            return this.root;
+        }
+        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+    
+    // Start of function levelOrder
+    this.levelOrder = function(root) {
+
+         if (root === null) {
+            return;
+        }
+
+        const queue = [];
+        queue.push(root);
+
+        const result = [];
+
+        while (queue.length > 0) {
+            const currentNode = queue.shift(); // dequeue 
+            result.push(currentNode.data);
+
+            if (currentNode.left !== null) {
+                queue.push(currentNode.left);
+            }
+            if (currentNode.right !== null) {
+                queue.push(currentNode.right);
+            }
+        }
+
+        console.log(result.join(' '));
+
+	}; // End of function levelOrder
+}; // End of function BinarySearchTree
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var _input = "";
+
+process.stdin.on('data', function (data) {
+    _input += data;
+});
+
+process.stdin.on('end', function () {
+    var tree = new BinarySearchTree();
+    var root = null;
+    
+    var values = _input.split('\n').map(Number);
+    
+    for (var i = 1; i < values.length; i++) {
+        root = tree.insert(root, values[i]);
+    }
+    
+    tree.levelOrder(root);
+});
