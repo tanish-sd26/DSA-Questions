d254166beed86651083eb4bf6b121fe812471c72
tanish-sd26 <mauryatanisha824@gmail.com>
2026-09-03 09:10:38 +0530

    Solve: Construct Uniform Parity Array II

---
+++ daily streak JS Questions/3876. ConstructUniformParityArrayII.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function(nums1) {
+    
+    let min = Math.min(...nums1);
+
+    if (min % 2 === 1) {
+        return true;
+    }
+
+    for (let num of nums1) {
+        if (num % 2 === 1) {
+            return false;
+        }
+    }
+
+    return true;
+};
