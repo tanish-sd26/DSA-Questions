a2d0a1cd769a267ef8663f72a7449e1669bb3909
tanish-sd26 <mauryatanisha824@gmail.com>
2026-09-05 17:35:34 +0530

    Solve: Smallest Stable Index II

---
+++ daily streak JS Questions/3904. SmallestStableIndexII.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var firstStableIndex = function(nums, k) {
+    
+    const n = nums.length;
+
+    const prefixMax = new Array(n);
+    const suffixMin = new Array(n);
+
+    // Prefix maximum
+    prefixMax[0] = nums[0];
+
+    for (let i = 1; i < n; i++) {
+        prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+    }
+
+    // Suffix minimum
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        suffixMin[i] = Math.min(suffixMin[i + 1], nums[i]);
+    }
+
+    // Find smallest stable index
+    for (let i = 0; i < n; i++) {
+        if (prefixMax[i] - suffixMin[i] <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+};
